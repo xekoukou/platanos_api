603098018141c93cb7d7cde8cd760e986591ba69
Apostolis Xekoukoulotakis <xekoukou@gmail.com>
2013-05-09 18:30:29 +0300

    ok

--- vertex.h
+++ vertex.h
@@ -48,19 +48,9 @@ vertex_t * vertex_dup(vertex_t *vertex);
 void vertex_deserialize(khash_t (vertices) * hash,zmsg_t *msg);
 
 //add frames to the msg
-zmsg_t * vertex_serialize(zmsg_t *msg,vertex_t *vertex);
-
-//iter should be at the beginning of the vertex
-// add frames to the prefix
-void vertex_db_to_msg(zmsg_t *prefix,leveldb_iterator_t *iter);
-
-// remove frames from the message
-void vertex_db_msg_to_db(dbo_t *dbo,zmsg_t *msg);
+zmsg_t * vertex_serialize(vertex_t *vertex);
 
 
-void vertex_db_destroy(dbo_t *dbo,leveldb_iterator_t *iter);
 
-//iterator is moved one vertex position
-void vertex_iter_next(leveldb_iterator_t *iter);
 
 #endif
